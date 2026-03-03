85e9830388840034d18f098ef3035bfbde34d6b1
harriets28 <harrietjsanderson@gmail.com>
2026-03-03 09:52:05 +0000

    Add user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('blog:home')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Welcome to Wanderlust, {user.username}!')
+            return redirect('blog:home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
