43e18c8a6bd0cd41591724736fd1c0f8e7b7cff3
harriets28 <harrietjsanderson@gmail.com>
2026-03-03 09:20:55 +0000

    Register models in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Post, Category, Comment, Like, UserProfile
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'destination', 'status', 'featured', 'created_at']
+    list_filter = ['status', 'featured', 'category']
+    search_fields = ['title', 'destination']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['status', 'featured']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created_at', 'approved']
+    list_editable = ['approved']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at']
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'location']
